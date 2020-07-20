313a0bbba9fff9f9c8ee7c4e7ccbf979b1a7e55d
MichalStachowski <40248580+MichalStachowski@users.noreply.github.com>
2020-07-20 19:14:38 +0200

    Update GUI

--- GUI/main.py
+++ GUI/main.py
@@ -1,77 +1,88 @@
-import sys, math
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QPushButton, QMessageBox
+import math
 import random
+from PyQt5 import QtGui, QtCore, QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow, QGraphicsScene, QGraphicsView,  QGraphicsPolygonItem, \
+    QPushButton, QMessageBox, QGraphicsPixmapItem
+from PyQt5.QtGui import QBrush, QPixmap
+from PyQt5.Qt import Qt
+import sys
+import rsc
 
 
-class MyWidget(QtWidgets.QWidget):
-    def __init__(self, parent=None):
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
         self.player_snake = my_snake
 
         # Zmienne wykorzystywane do przechowywania owoców
         self.apple_pos_list = []
         self.idx_apple_pos = []
 
-        # noinspection PyArgumentList
-        QtWidgets.QWidget.__init__(self, parent)
-        self.setGeometry(100, 100, 1000, 800)  # wymiary okna
-        self.init_buttons()  # Obsługa przycisków w GUI
+        self.title = "PyQt5 QGraphicView"
+        self.top = 200
+        self.left = 500
+        self.width = 1200
+        self.height = 800
 
-        # Obsługa labeli w GUI
-        self.label_first_snake_points = QtWidgets.QLabel(self)
-        self.label_second_snake_points = QtWidgets.QLabel(self)
-        self.label_whose_turn = QtWidgets.QLabel(self)
-        self.init_labels()
+        self.InitWindow()
 
-        self.pen = QtGui.QPen(QtGui.QColor(0, 0, 0))  # Kolor pisaka - czarny
-        self.pen.setWidth(3)  # ustawienie grubosci pisaka
-        self.brush = QtGui.QBrush(QtGui.QColor(255, 255, 255))  # ustawianie wypełnienia figur
+    def InitWindow(self):
+        self.setWindowIcon(QtGui.QIcon("icon.png"))
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
 
-        board.init_board()  # Inicjalizacja planszy
+        self.createGraphicView()
 
-    def init_buttons(self):
-        """
-        Obsługa przycisków w GUI
-        """
         # Przycisk odpowiedzialny za nową grę
         b_new_game = QPushButton('Nowa Gra', self)
-        b_new_game.move(100, 50)
+        b_new_game.move(10, 10)
         b_new_game.clicked.connect(self.click_new_game)
 
         # Przycisk odpowiedzialny za wyjście
         b_exit = QPushButton('Wyjscie', self)
-        b_exit.move(200, 50)
+        b_exit.move(110, 10)
         b_exit.clicked.connect(self.click_exit)
 
         # Przycisk odpowiedzialny za ruch w lewy górny róg
         b_q = QPushButton('Lewy Górny', self)
-        b_q.move(100, 700)
+        b_q.move(10, 70)
         b_q.clicked.connect(self.click_q)
 
         # Przycisk odpowiedzialny za ruch w górę
         b_w = QPushButton('Góra', self)
-        b_w.move(200, 700)
+        b_w.move(110, 70)
         b_w.clicked.connect(self.click_w)
 
         # Przycisk odpowiedzialny za ruch w prawy górny róg
         b_e = QPushButton('Prawy Górny', self)
-        b_e.move(300, 700)
+        b_e.move(210, 70)
         b_e.clicked.connect(self.click_e)
 
         # Przycisk odpowiedzialny za ruch w lewy dolny róg
         b_a = QPushButton('Lewy Dolny', self)
-        b_a.move(100, 750)
+        b_a.move(10, 100)
         b_a.clicked.connect(self.click_a)
 
         # Przycisk odpowiedzialny za ruch w dół
         b_s = QPushButton('Dół', self)
-        b_s.move(200, 750)
+        b_s.move(110, 100)
         b_s.clicked.connect(self.click_s)
 
         # Przycisk odpowiedzialny za ruch w prawy dolny róg
         b_d = QPushButton('Prawy Dolny', self)
-        b_d.move(300, 750)
+        b_d.move(210, 100)
         b_d.clicked.connect(self.click_d)
+
+        # noinspection PyAttributeOutsideInit
+        self.label_first_snake_points = QtWidgets.QLabel(self)
+        # noinspection PyAttributeOutsideInit
+        self.label_second_snake_points = QtWidgets.QLabel(self)
+        # noinspection PyAttributeOutsideInit
+        self.label_whose_turn = QtWidgets.QLabel(self)
+        self.init_labels()
+
+        self.show()
 
     def init_labels(self):
         """
@@ -102,12 +113,100 @@ class MyWidget(QtWidgets.QWidget):
             self.label_whose_turn.setText("Tura: Gracz2")
         self.label_whose_turn.adjustSize()
 
-    def paintEvent(self, event):
+    # noinspection PyAttributeOutsideInit
+    def createGraphicView(self):
+        self.scene = QGraphicsScene()
+        self.greenBrush = QBrush(Qt.green)
+        self.grayBrush = QBrush(Qt.gray)
+
+        self.pen = QtGui.QPen(QtGui.QColor(0, 0, 0))
+        self.pen.setWidth(3)
+
+        graphicView = QGraphicsView(self.scene, self)
+        graphicView.setGeometry(50, 150, 1000, 600)
+
+        # osobna scena zeby wyswietlic zdjecie korzystajac QGraphicsItem
+        self.scene_img = QGraphicsScene()
+        graphicView_img = QGraphicsView(self.scene_img, self)
+        graphicView_img.setGeometry(1000, 10, 120, 120)
+
+        self.shapes()
+
+    def shapes(self):
+        """
+        Tworzę itemy
+        :return:
+        """
+        w = 0
+        k = 0
+        idx_counter = 0
+
+        for r in range(-5, 5, 1):
+            for c in range(-5, 5, 1):
+                he = HexagonItem(6, 30, (-500) + 90*c, (-300) + r*50, w, k, idx_counter)
+                he.create_poly()
+                hex_items.append(he)
+                self.scene.addItem(hex_items[idx_counter])
+                idx_counter += 1
+                k += 2
+
+            w += 1
+            k = 1
+
+            for c in range(-5, 5, 1):
+                he = HexagonItem(6, 30, (-500) + 45 + (90*c), (-300) + (r*50) + 25, w, k, idx_counter)
+                he.create_poly()
+                hex_items.append(he)
+                self.scene.addItem(hex_items[idx_counter])
+                idx_counter += 1
+                k += 2
+
+            w += 1
+            k = 0
+
+        # Użycie resources za pomoca QGraphicsPixmapItem do wyświetlania grafik
+        im_qrc = QGraphicsPixmapItem()
+        path = QPixmap(":/mojqrc/sn_img.jpg")
+        im_qrc.setPixmap(path)
+        self.scene_img.addItem(im_qrc)
+
+    # noinspection PyAttributeOutsideInit
+    def paintEvent(self, *args, **kwargs):
+        self.pen = QtGui.QPen(QtGui.QColor(0, 0, 0))
+        self.pen.setWidth(3)
+        self.greenBrush = QtGui.QBrush(QtGui.QColor(100, 255, 100))
+        self.redBrush = QtGui.QBrush(QtGui.QColor(255, 100, 100))
+        self.blackBrush = QtGui.QBrush(QtGui.QColor(10, 10, 10))
+        self.brush = QtGui.QBrush(QtGui.QColor(255, 255, 255))
+
         painter = QtGui.QPainter(self)
         painter.setPen(self.pen)
         painter.setBrush(self.brush)
 
-        board.draw_board(painter)
+        # Losuje jablka
+        if not self.apple_pos_list:
+            for _ in range(5):
+                random_idx = random.randint(0, len(hex_items)-1)
+                a_x = hex_items[random_idx].w
+                a_y = hex_items[random_idx].k
+                if [a_x, a_y] in my_snake.snake_pos or [a_x, a_y] in enemy_snake.snake_pos:
+                    pass
+                else:
+                    self.apple_pos_list.append([a_x, a_y])
+                    self.idx_apple_pos.append(random_idx)
+
+        # Dobranie odpowiedniego wypelnienia
+        counter = 0
+        idx_my_snake.clear()
+        idx_enemy_snake.clear()
+        for hex_item in hex_items:
+            if [hex_item.w, hex_item.k] in my_snake.snake_pos:
+                idx_my_snake.append(hex_items.index(hex_item))
+            elif [hex_item.w, hex_item.k] in enemy_snake.snake_pos:
+                idx_enemy_snake.append(hex_items.index(hex_item))
+
+        counter += 1
+        self.scene.update()
 
     def change_player_snake(self):
         """
@@ -135,10 +234,11 @@ class MyWidget(QtWidgets.QWidget):
         self.label_first_snake_points.setText(f"Gracz1: {my_snake.points}")
         self.label_second_snake_points.setText(f"Gracz2: {enemy_snake.points}")
         self.label_whose_turn.setText("Tura: Gracz1")
-        self.update()
+        self.scene.update()
 
+    # noinspection PyMethodMayBeStatic
     def click_exit(self):
-        sys.exit(app.exec_())
+        sys.exit(App.exec_())
 
     def click_q(self):
         self.player_snake.move("Q", self.apple_pos_list, self.idx_apple_pos)
@@ -171,7 +271,7 @@ class MyWidget(QtWidgets.QWidget):
         msg = QMessageBox()
         msg.setWindowTitle("Niedozwolony ruch")
         msg.setText("Wąż nie może najechać na swój ogon")
-        msg.setIcon(QMessageBox.Critical)  # Warning / Critical / Information / Question
+        msg.setIcon(QMessageBox.Critical)
         msg.setStandardButtons(QMessageBox.Ok)
         msg.setDefaultButton(QMessageBox.Ok)
         msg.setInformativeText("Koniec gry")
@@ -182,7 +282,7 @@ class MyWidget(QtWidgets.QWidget):
             msg.setDetailedText("Wygrywa Gracz 1")
 
         msg.buttonClicked.connect(self.exit_snake)
-        x = msg.exec_()  # this shows the message
+        x = msg.exec_()
 
     def snake_moved_outside_board_popup(self):
         """
@@ -191,7 +291,7 @@ class MyWidget(QtWidgets.QWidget):
         msg = QMessageBox()
         msg.setWindowTitle("Niedozwolony ruch")
         msg.setText("Wąż wyszedł poza pole gry")
-        msg.setIcon(QMessageBox.Critical)  # Warning / Critical / Information / Question
+        msg.setIcon(QMessageBox.Critical)
         msg.setStandardButtons(QMessageBox.Ok)
         msg.setDefaultButton(QMessageBox.Ok)
         msg.setInformativeText("Koniec gry")
@@ -202,7 +302,7 @@ class MyWidget(QtWidgets.QWidget):
             msg.setDetailedText("Wygrywa Gracz 1")
 
         msg.buttonClicked.connect(self.exit_snake)
-        x = msg.exec_()  # this shows the message
+        x = msg.exec_()
 
     def snake_on_snake_popup(self):
         """
@@ -211,7 +311,7 @@ class MyWidget(QtWidgets.QWidget):
         msg = QMessageBox()
         msg.setWindowTitle("Niedozwolony ruch")
         msg.setText("Wąż najechał na węża przeciwnika")
-        msg.setIcon(QMessageBox.Critical)  # Warning / Critical / Information / Question
+        msg.setIcon(QMessageBox.Critical)
         msg.setStandardButtons(QMessageBox.Ok)
         msg.setDefaultButton(QMessageBox.Ok)
         msg.setInformativeText("Koniec gry")
@@ -222,7 +322,7 @@ class MyWidget(QtWidgets.QWidget):
             msg.setDetailedText("Wygrywa Gracz 1")
 
         msg.buttonClicked.connect(self.exit_snake)
-        x = msg.exec_()  # this shows the message
+        x = msg.exec_()
 
     def player_won_popup(self):
         """
@@ -234,13 +334,14 @@ class MyWidget(QtWidgets.QWidget):
             msg.setText("Gracz 1 zdobył 10 punktów")
         else:
             msg.setText("Gracz 2 zdobył 10 punktów")
-        msg.setIcon(QMessageBox.Information)  # Warning / Critical / Information / Question
+        msg.setIcon(QMessageBox.Information)
         msg.setStandardButtons(QMessageBox.Ok)
         msg.setDefaultButton(QMessageBox.Ok)
         msg.setInformativeText("Gratulacje!")
         msg.buttonClicked.connect(self.exit_snake)
-        x = msg.exec_()  # this shows the message
+        x = msg.exec_()
 
+    # noinspection PyMethodMayBeStatic
     def exit_snake(self, i):
         exit()
 
@@ -252,13 +353,55 @@ class MyWidget(QtWidgets.QWidget):
         self.label_second_snake_points.setText(f"Gracz2: {enemy_snake.points}")
 
 
-class HexField:
-    def __init__(self, r, c, idx):
+class HexagonItem(QGraphicsPolygonItem):
+    """
+    Klasa ktora definiuje kazde pole na planszy. Tworzenie createpoly i rysowanie paint
+    """
+    def __init__(self, n, rad, pos_x, pos_y, w, k, idx_counter):
+        super().__init__()
+        self.n = n
+        self.rad = rad
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.w = w
+        self.k = k
+        self.idx = idx_counter
+        self.polygon = None
         self.is_snake = False
         self.is_apple = False
-        self.w = r
-        self.k = c
-        self.idx = idx
+
+    def create_poly(self):
+        self.polygon = QtGui.QPolygonF()
+        w = 360 / self.n  # kąt o jaki przesunięte są wierzchołki
+
+        # Wyznaczanie punktów
+        for i in range(self.n):
+            t = w * i
+            x = self.rad * math.cos(math.radians(t))
+            y = self.rad * math.sin(math.radians(t))
+            self.polygon.append(QtCore.QPointF(self.pos_x + (1000 / 2 + x), self.pos_y + (600 / 2 + y)))
+
+        return self.polygon
+
+    # noinspection PyAttributeOutsideInit
+    def paint(self, QPainter, QStyleOptionGraphicsItem, widget=None):
+        self.pen = QtGui.QPen(QtGui.QColor(0, 0, 0))
+        self.pen.setWidth(3)
+        self.greenBrush = QtGui.QBrush(QtGui.QColor(100, 255, 100))
+        self.redBrush = QtGui.QBrush(QtGui.QColor(255, 100, 100))
+        self.blackBrush = QtGui.QBrush(QtGui.QColor(10, 10, 10))
+        self.brush = QtGui.QBrush(QtGui.QColor(255, 255, 255))
+        QPainter.setPen(self.pen)
+
+        # Wybranie odpowiedniego wypelnienia przy rysowaniu
+        if self.idx in idx_my_snake:
+            QPainter.setBrush(self.greenBrush)
+        elif self.idx in idx_enemy_snake:
+            QPainter.setBrush(self.blackBrush)
+        elif self.idx in window.idx_apple_pos:
+            QPainter.setBrush(self.redBrush)
+
+        QPainter.drawPolygon(self.polygon)
 
     def update_is_snake(self, player_snake):
         """
@@ -286,6 +429,9 @@ class HexField:
 
 
 class Snake:
+    """
+    Klasa definiująca węża - gracza
+    """
     def __init__(self, head):
         self.head = head
         self.snake_pos = [self.head]
@@ -335,12 +481,12 @@ class Snake:
             del idx_apple_pos[idx_to_remove]
 
             # aktualizacja ze na danym polu znajduje się snake
-            for hex_field in hex_fields:
+            for hex_field in hex_items:
                 hex_field.update_is_snake(self)
 
             # Aktualizacja wyniku punktowego
             self.points += 1
-            widget.update_points_label()
+            window.update_points_label()
         else:
             del self.snake_pos[-1]  # usuwam ostatnią pozycję na jakiej znalazł się wąż
 
@@ -348,126 +494,19 @@ class Snake:
         unique = list(set(map(tuple, self.snake_pos)))
         if len(unique) != len(self.snake_pos):
             print("NIEPRAWIDLOWY RUCH! KONIEC GRY")
-            widget.snake_eat_himself_popup()
+            window.snake_eat_himself_popup()
 
         # Mechanizm kontroli czy wąż nie wyszedł poza planszę
         if x < 0 or x > 19 or y < 0 or y > 19:
-            widget.snake_moved_outside_board_popup()  # popup z informacją o błędnym ruchu
+            window.snake_moved_outside_board_popup()  # popup z informacją o błędnym ruchu
 
         # Mechanizm kontroli czy doszło do kolizji węży
         if snake_on_snake_event():
-            widget.snake_on_snake_popup()
+            window.snake_on_snake_popup()
 
         # Mechanizm wyłonienia zwycięzcy po zdobyciu 10 punktów
         if self.points == 10:
-            widget.player_won_popup()
-
-        widget.update()
-
-
-class HexBoard:
-    def __init__(self, rows_n, cols_n):
-        self.rows_num = rows_n
-        self.cols_num = cols_n
-        self.w = 0
-        self.k = 0
-        self.idx_counter = 0
-        self.polygon = None
-
-    def init_board(self):
-        """
-        Generowanie planszy oraz listy instancji obiektów
-        """
-        for r in range(-5, 5, 1):
-            for c in range(-5, 5, 1):
-                self.polygon = self.draw_hexagon(6, 30, 90 * c, r * 50)
-                polygons_list.append(self.polygon)
-                hex_fields.append(HexField(self.w, self.k, self.idx_counter))
-                self.idx_counter += 1
-                self.k += 2
-
-            self.w += 1
-            self.k = 1
-
-            for c in range(-5, 5, 1):
-                self.polygon = self.draw_hexagon(6, 30, 45 + (90 * c), (r * 50) + 25)
-                polygons_list.append(self.polygon)
-                hex_fields.append(HexField(self.w, self.k, self.idx_counter))
-                self.idx_counter += 1
-                self.k += 2
-
-            self.w += 1
-            self.k = 0
-
-    def draw_hexagon(self, n, rad, pos_x, pos_y):
-        """
-        Wyrysowanie pojedynczego pola
-        :param n: Liczba wierzchołków
-        :param rad: Promień sześciokąta
-        :param pos_x: Współrzędna x środka sześciokąta
-        :param pos_y: Współrzędna x środka sześciokąta
-        """
-        polygon = QtGui.QPolygonF()
-        w = 360 / n  # kąt o jaki przesunięte są wierzchołki
-
-        # Wyznaczanie punktów
-        for i in range(n):
-            t = w * i
-            x = rad * math.cos(math.radians(t))
-            y = rad * math.sin(math.radians(t))
-            polygon.append(QtCore.QPointF(pos_x + (1000 / 2 + x), pos_y + (800 / 2 + y)))
-
-        return polygon
-
-    def draw_board(self, painter):
-        """
-        Wyrysowanie planszy w każdej iteracji
-        :param painter: Pisak wykorzystywany do rysowania (zdefiniowany w init klasy MyWidget)
-        """
-        counter = 0  # licznik który wykorzystywany jest do rysowania
-
-        # Patrze na których polach znajduje się wąż
-        idx_mysnake_pos = []
-        idx_enemysnake_pos = []
-        for hex_field in hex_fields:
-            hex_field.update_is_snake(my_snake)  # czy na tym polu jest waz
-            if hex_field.is_snake:  # jezeli jest
-                idx_mysnake_pos.append(hex_fields.index(hex_field))
-
-            # dla weza przeciwnika (zeby oddzielic je kolorem)
-            hex_field.update_is_snake(enemy_snake)
-            if hex_field.is_snake:
-                idx_enemysnake_pos.append(hex_fields.index(hex_field))
-
-        # LOSOWANIE OWOCÓW
-        if not widget.apple_pos_list:
-            for _ in range(5):
-                random_index = random.randint(0, len(hex_fields))
-                for hex_field in hex_fields:
-                    if hex_field.idx == random_index:
-                        # jezeli na danym polu nie znajduje się snake na polu bedzie jablko
-                        flag_random_apple = hex_field.update_into_apple()
-                        if flag_random_apple:
-                            widget.apple_pos_list.append([hex_field.w, hex_field.k])
-                            widget.idx_apple_pos.append(hex_field.idx)
-
-        # Wypełnienie sześciokąta kolorem odpowiednim dla danego obiektu
-        for polygon in polygons_list:
-            if counter in idx_mysnake_pos:
-                self.brush = QtGui.QBrush(QtGui.QColor(100, 255, 100))  # Kolor zielony - Wąż nr 1
-                painter.setBrush(self.brush)
-            elif counter in idx_enemysnake_pos:
-                self.brush = QtGui.QBrush(QtGui.QColor(10, 10, 10))  # Kolor czarny - Wąż nr 2
-                painter.setBrush(self.brush)
-            elif counter in widget.idx_apple_pos:
-                self.brush = QtGui.QBrush(QtGui.QColor(255, 100, 100))  # Czerwony kolor - Jabłko
-                painter.setBrush(self.brush)
-            else:
-                self.brush = QtGui.QBrush(QtGui.QColor(255, 255, 255))  # Biały kolor - Puste pole
-                painter.setBrush(self.brush)
-            painter.drawPolygon(polygon)
-
-            counter += 1
+            window.player_won_popup()
 
 
 def snake_on_snake_event():
@@ -481,15 +520,11 @@ def snake_on_snake_event():
         return True
 
 
-hex_fields = []
-polygons_list = []
+hex_items = []
+idx_my_snake = []
+idx_enemy_snake = []
 my_snake = Snake([2, 2])
 enemy_snake = Snake([17, 17])
-board = HexBoard(10, 10)
-
-app = QtWidgets.QApplication(sys.argv)
-
-widget = MyWidget()
-widget.show()
-
-sys.exit(app.exec_())
+App = QApplication(sys.argv)
+window = Window()
+sys.exit(App.exec())
